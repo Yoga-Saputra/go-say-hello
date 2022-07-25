816a2ff2f7dd0184f2100f24ce867894bdd2a40e
Yoga-Saputra <yogasaputran@gmail.com>
2022-07-25 22:00:12 +0700

    create module say hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yoga-Saputra/go-say-hello
+
+go 1.18
---
+++ say_hello.go
@@ -0,0 +1,7 @@
+package go_say_hello
+
+// create module : go mod init github.com/Yoga-Saputra/go-say-hello
+
+func SayHello() string {
+	return "Hello"
+}
